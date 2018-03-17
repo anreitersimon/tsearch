9d5a08167532d1b2f47cbdb0bcedb3f041a5f969
smon <simon.anreiter@i-mobility.at>
2018-03-17 22:28:04 +0100

    adds interactive list

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ var Ora = require("ora");
 var UI = require("./ui");
 
 var availableProviders = [eztv];
+var query = "";
 
 program
-  .option("-q, --query <query>", "query", "")
+  .arguments("<query>")
   .option("-c, --category <category>", "category", "")
   .option("-l, --limit <limit>", "limit", 20)
   .option(
@@ -20,25 +21,28 @@ program
     /^(eztv)$/i,
     "eztv"
   )
-  .parse(process.argv);
+  .action(function(cmd) {
+    query = cmd;
+  });
+
+program.parse(process.argv);
 
 provider = availableProviders.find(it => {
   return it.name == program.provider;
 });
 
 const spinner = new Ora({
-  text: chalk`{blue Searching for} {red "${program.query}"} {blue on} {red ${
+  text: chalk`{blue Searching for} {red "${query}"} {blue on} {red ${
     provider.name
   }}`
 });
 spinner.start();
 
-provider.search(program.query, "", 100).then(
+provider.search(query, "", 100).then(
   data => {
     spinner.succeed();
-    UI.present(data, program.limit);
-    UI.selectTorrents(data, selected => {
-      selected.forEach(t => openTorrent(t));
+    UI.present(data, selected => {
+      openTorrent(selected);
     });
   },
   function(err) {
@@ -50,7 +54,7 @@ provider.search(program.query, "", 100).then(
 const spawnSync = require("child_process").spawnSync;
 
 function openTorrent(torrent) {
-  const result = spawnSync("open", [torrent.torrent_link], {
+  const result = spawnSync("open", ["-g", torrent.torrent_link], {
     timeout: 60000
   });
 
--- ui.js
+++ ui.js
@@ -1,73 +1,59 @@
-var Table = require("cli-table");
 var chalk = require("chalk");
 var prompt = require("prompt");
+var List = require("term-list");
 
 module.exports = {
-  present: function(data, limit) {
+  present: function(data, selected) {
     if (data.length == 0) {
       return;
     }
 
     console.log(chalk`{green ${data.length}} results:`);
 
-    var table = new Table({
-      head: [
-        chalk.bold("#"),
-        chalk.bold.blue("Name"),
-        chalk.green("\u25B2"),
-        chalk.red("\u25BC"),
-        chalk.blue("Added")
-      ],
-      colWidths: [6, 50, 8, 8, 12]
-    });
-
-    data.slice(0, limit).forEach(torrent => {
-      table.push([
-        torrent.torrent_num,
-        chalk.blue(torrent.title.substring(0, 48)),
-        chalk.green(format(torrent.seeds)),
-        chalk.red(format(torrent.leechs)),
-        chalk.bold(format(torrent.date_added)),
-      ]);
-    });
-
-    console.log(table.toString());
-
-    console.log("");
-  },
-  selectTorrents: function(data, callback) {
-    var stdin = process.openStdin();
-
-    prompt.message = "select torrent(s) to download";
-
-    prompt.start();
-
-    prompt.get(
-      [
-        {
-          name: "selection",
-          type: "string"
-        }
-      ],
-      (err, result) => {
-        var selectedTorrents = result.selection.split(",");
-
-        var selected = data.filter(torrent => {
-          return selectedTorrents.includes(String(torrent.torrent_num));
-        });
-
-        var selectedNames = selected
-          .map(torrent => {
-            return "- " + chalk.blue(torrent.title);
-          })
-          .join("\n");
-
-        console.log("selected:");
-        console.log(selectedNames);
-
-        callback(selected);
-      }
+    console.log(
+      formatRow(
+        [
+          chalk.bold("#"),
+          chalk.bold.blue("Name"),
+          chalk.green("\u25B2"),
+          chalk.red("\u25BC"),
+          chalk.blue("Added")
+        ],
+        []
+      )
     );
+
+    var list = new List({ marker: "\033[36m› \033[0m", markerLength: 2 });
+
+    data.forEach(torrent => {
+      var text = formatRow(
+        [
+          torrent.torrent_num,
+          chalk.blue(torrent.title.substring(0, 48)),
+          chalk.green(format(torrent.seeds)),
+          chalk.red(format(torrent.leechs)),
+          chalk.bold(format(torrent.date_added))
+        ],
+        []
+      );
+      list.add(torrent, text);
+    });
+
+    list.start();
+    list.on("keypress", function(key, item) {
+      switch (key.name) {
+        case "return":
+          selected(item);
+          break;
+        case "backspace":
+          list.remove(list.selected);
+          break;
+      }
+    });
+
+    list.on("empty", function() {
+      list.stop();
+    });
   }
 };
 
@@ -77,4 +63,11 @@ function format(val) {
   } else {
     return "-";
   }
+}
+
+function formatRow(data, colWidths) {
+  var result = "";
+  console.log('dsads')
+
+  return data.join(" | ");
 }
